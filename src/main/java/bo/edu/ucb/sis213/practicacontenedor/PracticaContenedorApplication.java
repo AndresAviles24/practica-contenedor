1b27872c4d8c5c0c2676092241362cc53d7d76b0
AndresAviles24 <130066347+AndresAviles24@users.noreply.github.com>
2023-09-14 19:57:53 -0400

    initial commit

---
+++ src/main/java/bo/edu/ucb/sis213/practicacontenedor/PracticaContenedorApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis213.practicacontenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaContenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaContenedorApplication.class, args);
+	}
+
+}
